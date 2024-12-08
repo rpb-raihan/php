3ceb249c3fa357591e64db00d2f56de57d69c5a6
Raihan Parvez <123397195+rpb-raihan@users.noreply.github.com>
2024-12-08 11:32:40 +0600

    Add files via upload
    Two input filed and a submission button using get post

---
+++ 01.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="welcome.php" method="post">
+    <h1>Get and Post</h1>
+ <input type="text" name=“name"><br>
+ <input type="text" name="email"><br>
+<input type="submit">
+</form>
+</body>
+</html>
